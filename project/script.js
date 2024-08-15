f6254c1ad607f6f9620ffdfeb475e5197303b611
Maysa Lima <88833928+maysa2504@users.noreply.github.com>
2024-08-15 20:52:51 -0300

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,86 +1,77 @@
 const inputTexto = document.querySelector(".input");
-const message = document.querySelector(".message");
+const outputMessage = document.querySelector(".message");
+const noMessageText = document.querySelector(".texto1"); 
+const noMessageSubText = document.querySelector(".texto2"); 
+const imageElement = document.querySelector(".aside_message img"); 
 
 function btnEncriptar() {
-    const textEncriptada = encriptar(inputTexto.value);
-    message.value = textEncriptada;
-    message.style.backgroundImage = "none";
+    const textoEncriptado = encriptar(inputTexto.value);
+    outputMessage.value = textoEncriptado;
+    outputMessage.style.backgroundImage = "none";
+    checkOutputMessage();
 }
 
-function encriptar(stringEncriptada) {
-    let matrixCode = [
+function encriptar(texto) {
+    const matrizCodigo = [
         ["e", "enter"],
         ["i", "imes"],
         ["a", "ai"],
         ["o", "ober"],
         ["u", "ufat"]
     ];
-    stringEncriptada = stringEncriptada.toLowerCase();
-    for (let i = 0; i < matrixCode.length; i++) {
-        if (stringEncriptada.includes(matrixCode[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(matrixCode[i][0], matrixCode[i][1]);
+    texto = texto.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (texto.includes(matrizCodigo[i][0])) {
+            texto = texto.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
     }
-    return stringEncriptada;
+    return texto;
 }
 
 function btnDesencriptar() {
-    const textDesencriptado = desencriptar(inputTexto.value);
-    message.value = textDesencriptado;
-    message.style.backgroundImage = "none"; 
+    const textoDesencriptado = desencriptar(inputTexto.value);
+    outputMessage.value = textoDesencriptado;
+    outputMessage.style.backgroundImage = "none"; 
+    checkOutputMessage();
 }
 
-function desencriptar(strDesencriptado) {
-    let matrixCode = [
+function desencriptar(texto) {
+    const matrizCodigo = [
         ["a", "ai"],
         ["e", "enter"],
         ["i", "imes"],
         ["o", "ober"],
         ["u", "ufat"]
     ];
-    strDesencriptado = strDesencriptado.toLowerCase();
-    for (let i = 0; i < matrixCode.length; i++) {
-        if (strDesencriptado.includes(matrixCode[i][1])) {
-            strDesencriptado = strDesencriptado.replaceAll(matrixCode[i][1], matrixCode[i][0]);
+    texto = texto.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (texto.includes(matrizCodigo[i][1])) {
+            texto = texto.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
         }
     }
-    return strDesencriptado;
+    return texto;
 }
 
 function btnCopy() {
-    const copiedText = document.querySelector(".message");
-    copiedText.select();
-    navigator.clipboard.writeText(copiedText.value).then(() => {
+    const textoCopiado = document.querySelector(".message");
+    textoCopiado.select();
+    navigator.clipboard.writeText(textoCopiado.value).then(() => {
         alert("Texto copiado para a área de transferência!");
     }).catch(err => {
         console.error("Erro ao copiar texto: ", err);
     });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*mudar codigo 
-
-
-
-window.onload = function() {
-    document.getElementsByClassName("encriptar").addEventListener("click",function() {
-        var meuParagrafo = document.getElementsByClassName("texto1")
-        meuParagrafo.innerHTML = " voltar a inicio da sessão ..."
-    })
-}*/
+function checkOutputMessage() {
+    if (outputMessage.value !== "") {
+        noMessageText.style.display = "none";
+        noMessageSubText.style.display = "none";
+        imageElement.style.display = "none";
+    } else {
+        noMessageText.style.display = "block";
+        noMessageSubText.style.display = "block";
+        imageElement.style.display = "block";
+    }
+}
